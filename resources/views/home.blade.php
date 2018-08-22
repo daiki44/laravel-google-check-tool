a53e368337899c847ebbfdc5fdde4631466d3577
daiki.sekiguchi <sekiseki0213@gmail.com>
2018-08-23 00:18:55 +0900

    [add] google analyticsの追加

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -67,5 +67,18 @@ function requestSearch() {
     });
 }
 </script>
+
+@if (config('app.env') === 'production')
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-124458268-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-124458268-1');
+</script>
+@endif
+
 </body>
 </html>
